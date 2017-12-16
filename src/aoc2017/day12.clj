413d1304ac76f30b8387c9cce53b589706745040
Valentin Waeselynck <val@bandsquare.fr>
2017-12-16 14:06:09 +0100

    2017 days 12 and 15

--- src/aoc2017/day12.clj
+++ src/aoc2017/day12.clj
@@ -1,4 +1,6 @@
-(ns aoc2017.day12)
+(ns aoc2017.day12
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 ;--- Day 12: Digital Plumber ---
 ;Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some programs can't communicate with each other.
@@ -32,3 +34,85 @@
 ;
 ;How many programs are in the group that contains program ID 0?
 ;
+
+(defn parse [input]
+  (->> input
+    (str/split-lines)
+    (map (fn [l] (read-string (str "[" l "]"))))
+    (mapv (fn [[v _ & neighbours]]
+            [v neighbours]))))
+
+(comment
+  (def parsed (parse (slurp (io/resource "aoc2017/day12.txt"))))
+  )
+
+(defn adjacency-list
+  [parsed]
+  (->> parsed
+    (mapcat (fn [[v neighbours]]
+              (for [n neighbours]
+                [v n])))
+    (reduce
+      (fn [al [v n]]
+        (if (not= v n)
+          (-> al
+            (update v #(-> % (or #{}) (conj n)))
+            (update n #(-> % (or #{}) (conj v))))
+          al))
+      {})))
+
+(comment
+  (adjacency-list parsed)
+  )
+
+(defn walk
+  [al state v]
+  (if (contains? state v)
+    state
+    (let [neighbours (get al v #{})]
+      (as-> state state
+        (assoc state v ::visiting)
+        (reduce #(walk al %1 %2) state neighbours)
+        (assoc state
+          v
+          (->> neighbours
+            (map state)
+            (remove #{::visiting})
+            (reduce into (conj neighbours v))))))))
+
+(defn find-reachable
+  [al v0]
+  (get (walk al {} v0) v0))
+
+(defn solve1
+  [parsed]
+  (count (find-reachable (adjacency-list parsed) 0)))
+
+(comment
+  (solve1 parsed)
+  => 288)
+;
+;--- Part Two ---
+;There are more programs than just the ones in the group containing program ID 0. The rest of them have no way of reaching that group, and still might have no way of reaching each other.
+;
+;A group is a collection of programs that can all communicate via pipes either directly or indirectly. The programs you identified just a moment ago are all part of the same group. Now, they would like you to determine the total number of groups.
+;
+;In the example above, there were 2 groups: one consisting of programs 0,2,3,4,5,6, and the other consisting solely of program 1.
+;
+;How many groups are there in total?
+;
+
+(defn solve2
+  [parsed]
+  (let [al (adjacency-list parsed)]
+    (->> parsed
+      (map (fn [[v _]]
+             [v (apply min (get (walk al {} v) v))]))
+      (map second)
+      distinct)))
+(comment
+  (solve2 parsed)
+  => 211)
+
+
+
