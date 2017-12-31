20bdcd593d762f6c3b7951ea1d4a6d13405a1eb9
Valentin Waeselynck <val@bandsquare.fr>
2017-12-31 12:28:08 +0100

    day 19 part 1

--- src/aoc2017/day19.clj
+++ src/aoc2017/day19.clj
@@ -1,4 +1,6 @@
-(ns aoc2017.day19)
+(ns aoc2017.day19
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 ;--- Day 19: A Series of Tubes ---
 ;Somehow, a network packet got lost and ended up here. It's trying to follow a routing diagram (your puzzle input),
@@ -30,3 +32,128 @@
 ;The little packet looks up at you, hoping you can help it find the way. What letters will it see
 ;(in the order it would see them) if it follows the path?
 ;(The routing diagram is very wide; make sure you view it without line wrapping.)
+
+(defn parse-grid
+  [input]
+  (->> input
+    (str/split-lines)
+    (mapv vec)))
+
+(comment
+  (def input "     |\n     |  +--+\n     A  |  C\n F---|----E|--+\n     |  |  |  D\n     +B-+  +--+")
+
+  (def input (slurp (io/resource "aoc2017/day19.txt")))
+  (def grid (parse-grid input))
+  )
+
+(defn cell-value
+  [grid pos]
+  (let [[i j] pos]
+    (-> grid (get i) (get j \space))))
+
+(def letter?
+  (set "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+(defn v-axis
+  [v]
+  (case v
+    \| :vertical
+    \- :horizontal
+    nil))
+
+(defn dir-axis
+  [[i j]]
+  (if (zero? i)
+    :horizontal
+    :vertical))
+
+(defn turn-right
+  [[i j]]
+  [j (- i)])
+
+(defn turn-left
+  [[i j]]
+  [(- j) i])
+
+(defn move-by-dir
+  [[ip jp] [id jd]]
+  [(+ ip id) (+ jp jd)])
+
+(defn start-pos
+  [grid]
+  [0
+   (let [row (get grid 0)]
+     (->> (range (count row))
+       (filter (fn [j]
+                 (-> row (get j) (= \|))))
+       first))])
+
+(defn next-move
+  "returns the next direction to move by"
+  [grid current-pos current-dir]
+  (let [v (cell-value grid current-pos)]
+    (case v
+      (\| \-)
+      current-dir
+      \+
+      (let [next-dirs
+            (->> [(turn-right current-dir)
+                  (turn-left current-dir)]
+              (filter
+                (fn [nd]
+                  (not=
+                    (cell-value grid (move-by-dir current-pos nd))
+                    \space))))
+            cnd (count next-dirs)]
+        (cond
+          (= cnd 0)
+          (throw (ex-info "No turn option"
+                   {:current-pos current-pos :v v :current-dir current-dir}))
+          (> cnd 1)
+          (throw (ex-info "Too many turn options"
+                   {:current-pos current-pos :v v :current-dir current-dir}))
+          :else
+          (first next-dirs)))
+      \space
+      nil
+      (cond
+        (letter? v) current-dir
+        :else (throw (ex-info "Unknown case" {:current-pos current-pos :v v :current-dir current-dir}))))))
+
+(defn solve1
+  [grid]
+  (loop
+    [pos (start-pos grid)
+     dir [1 0]
+     letters []]
+    (let [v (cell-value grid pos)
+          letters (cond-> letters
+                    (letter? v) (conj v))]
+      (if-some [nd (next-move grid pos dir)]
+        (recur
+          (move-by-dir pos nd)
+          nd
+          letters)
+        (apply str letters)))))
+
+(defn print-subgrid
+  "for debugging"
+  [grid [i0 j0] [i1 j1]]
+  (->> (subvec grid i0 i1)
+    (mapv #(subvec % j0 j1))
+    (map #(apply str %))
+    (interpose "\n")
+    (apply str)
+    println))
+
+(comment
+
+  (print-subgrid
+    grid
+    [60 52] [66 58])
+
+  (solve1 grid)
+  => "DWNBGECOMY"
+
+  )
+
